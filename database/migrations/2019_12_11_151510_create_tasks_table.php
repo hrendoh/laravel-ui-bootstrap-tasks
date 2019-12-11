4982be7a72b5a7d4ab5922cc8a8ebe09ecbf8b9a
Hiroyuki Endoh <hrendoh@gmail.com>
2019-12-12 00:22:52 +0900

    モデルTaskとそのマイグレーションを追加

---
+++ database/migrations/2019_12_11_151510_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject');
+            $table->text('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->boolean('completed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
